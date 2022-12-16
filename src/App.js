73ab783353e25f2f91e62681149289dd8befa9b3
Elena Strakhova <elenastrakhova@gmail.com>
2022-12-15 19:12:25 -0500

    improved glow

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="d-flex justify-content-md-center align-items-center vh-100">
       <div className="centered">
-        <h1 className="display-4 fw-bold lh-1 glow">Merry Chrsitmas and Happy New Year!</h1>
+        <h1 className="display-4 fw-bold lh-1 opaque glow">Merry Chrsitmas and Happy New Year!</h1>
         <br></br>
         <br></br>
         <p className="opaque">Some of my all-time favorites - old and new, English and Russian, Christmas and New Year!</p>
