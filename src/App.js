5353e29fd0c125b97849ee4b6441885392d56967
Elena Strakhova <elenastrakhova@gmail.com>
2022-12-15 22:03:53 -0500

    xma

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="d-flex justify-content-md-center align-items-center vh-100">
       <div className="centered">
-        <h1 className="display-4 fw-bold lh-1">Merry Chrsitmas and Happy New Year!</h1>
+        <h1 className="display-4 fw-bold lh-1">Merry Christmas and Happy New Year!</h1>
         <br></br>
         <br></br>
         <p className="opaque fs-6 text fw-light">Some of my all-time favorites - old and new, English and Russian, Christmas and New Year!</p>
