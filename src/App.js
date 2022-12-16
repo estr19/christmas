064287267a9e3008fb57fcd07143aa91ba1d022d
Elena Strakhova <elenastrakhova@gmail.com>
2022-12-15 22:00:23 -0500

    design fix

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ function App() {
   return (
     <div className="d-flex justify-content-md-center align-items-center vh-100">
       <div className="centered">
-        <h1 className="display-4 fw-bold lh-1 opaque glow">Merry Chrsitmas and Happy New Year!</h1>
+        <h1 className="display-4 fw-bold lh-1">Merry Chrsitmas and Happy New Year!</h1>
         <br></br>
         <br></br>
-        <p className="opaque">Some of my all-time favorites - old and new, English and Russian, Christmas and New Year!</p>
+        <p className="opaque fs-6 text fw-light">Some of my all-time favorites - old and new, English and Russian, Christmas and New Year!</p>
         <br></br>
         <AudioPlayer tracks={tracks} />
       </div>
